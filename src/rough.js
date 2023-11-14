4006dbedd8d79fe29e2f03557df938b98b0caf4e
Amardeep Ranjan <amardeepranjan911@gmail.com>
2023-11-14 17:08:49 +0530

    intial commit

---
+++ src/rough.js
@@ -0,0 +1,44 @@
+var cars = {
+  label: "Autos",
+  subs: [
+    {
+      label: "SUVs",
+      subs: [],
+    },
+    {
+      label: "Trucks",
+      subs: [
+        {
+          label: "2 Wheel Drive",
+          subs: [],
+        },
+        {
+          label: "4 Wheel Drive",
+          subs: [
+            {
+              label: "Ford",
+              subs: [],
+            },
+            {
+              label: "Chevrolet",
+              subs: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Sedan",
+      subs: [],
+    },
+  ],
+};
+
+let x;
+console.log(x);
+
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
